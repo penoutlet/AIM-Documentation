3e047cd4fe73ce7f60f242d7d4b87adc1efe632c
Your Name <sjonesmusician@gmail.com>
2018-08-01 10:20:11 -0400

    fancy navigation added.

--- js.js
+++ js.js
@@ -16,3 +16,32 @@ $('#finalize-description-button').on('click',function(){
 	$('#finalize-description').toggle()
 
 })
+
+// ===================================================
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+		$('#mySidenav').toggle()
+    
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+$('#nav-button').on('click',function(){
+		$('#mySidenav').toggle()
+})
