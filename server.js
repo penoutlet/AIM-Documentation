81dfd65827fcecc0851b0a5fcccf3ce9cbd54e0b
Your Name <sjonesmusician@gmail.com>
2018-08-01 10:50:29 -0400

    style and js path changes... public folder added.

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express()
 var port = 3000 || process.env.port
 app.use(express.static('public'));
 app.get('/', function(req,res){
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/public/index.html')
 })
 app.get('/example',function(req,res){
-	res.sendFile(__dirname + '/example.html')
+	res.sendFile(__dirname + '/public/example.html')
 })
 app.get('/documentation',function(req,res){
-	res.sendFile(__dirname + '/documentation.html')
+	res.sendFile(__dirname + '/public/documentation.html')
 })
 
 app.listen(port, function(){
